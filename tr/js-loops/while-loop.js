cbedd020256ddecd89487b1f1889dfc1e984714d
Mustapha <m.shenkaia@gmail.com>
2023-06-14 11:11:09 +0300

    while loop eklendi

---
+++ tr/js-loops/while-loop.js
@@ -0,0 +1,17 @@
+// while-loop
+​
+let m=0;              // 1 tanimla
+while(m<21){          // 2 değerlendir
+    console.log(m);   // 3 uygula 
+    m++;              // 4 arttir  
+}
+
+//----------------------
+
+let n=0;
+while(n<21){
+   if(n%2==0){
+    console.log(n + ' çift sayi');
+   }
+    n++;
+}
