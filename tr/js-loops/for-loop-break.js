5c9d26b5d90f44d80beeaa82df1e32fcfdbbe0b8
Mustapha <m.shenkaia@gmail.com>
2023-06-14 11:17:29 +0300

    for loop break ve continue eklendi

---
+++ tr/js-loops/for-loop-break.js
@@ -0,0 +1,12 @@
+// break 
+// belirtilen koşulda durur. 
+for(let s=-4; s<5; s++){
+    let t=12;
+    if(s==0){
+        break;
+    } 
+    u=t/s;
+    console.log(u);
+}
+// -3, -4, -6, -12
+// s=0 değerine kadar çaliştir ve durdu.
---
+++ tr/js-loops/for-loop-continue.js
@@ -0,0 +1,13 @@
+// continue
+// belirtilen koşulu atlar. 
+for(let s=-4; s<5; s++){
+    let t=12;
+    if(s==0){
+        continue;
+    } 
+    u=t/s;
+    console.log(u);
+}
+//-3, -4, -6, -12, 12, 6, 4, 3
+// s=0 değerini atladi ve tüm değerleri çaliştirdi. 
+​
