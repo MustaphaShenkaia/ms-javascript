aefcf17c3503202226de2b522e0dabced3b7a14c
Mustapha <m.shenkaia@gmail.com>
2023-06-14 13:05:42 +0300

    json eklendi

---
+++ tr/js-json/json.js
@@ -0,0 +1,28 @@
+
+// JavaScript Object Notaion: JSON 
+
+/* 
+JSON farklı programlama dilleri kulanan platformlarla aynı format üzerinden bilgi alışverişi yapılmasını sağlayan bir notation dır. 
+veri gönderdiğimizde veya aldığımızda da JSON formatında olması kullanımını kolaylaştırır.
+​
+JSON 
+- "çift tırnak kullanılır",
+- veri "name":"value" pair leri, 
+- veriler virgüller ile ayrılır, 
+- {süslü parentez OBJECT ler için kullanılır},
+- [köşeli parantez ARRAY lar için kullanılır]
+​
+JSON data
+"name":"value" pair
+"firstName" : "Ahmet"
+​
+JSON Object
+{"firstName" : "Ahmet", "lastName":"Yilmaz"}
+​
+JSON Array
+"employee" : [
+   {"firstName" : "Ahmet", "lastName":"Yilmaz"},
+   {"firstName" : "Ayşe",  "lastName":"HiçYilmaz"}, 
+   {"firstName" : "Neşe",  "lastName":"Gün"} 
+]
+*/
